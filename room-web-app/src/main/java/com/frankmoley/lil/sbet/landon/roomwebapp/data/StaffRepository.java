a72e52aa05a77d3e5ebd0a04a628086ca30f66be
Emil Hristov <53617178+emodavitkov@users.noreply.github.com>
2024-02-11 17:53:32 +0100

    build a data repository

---
+++ room-web-app/src/main/java/com/frankmoley/lil/sbet/landon/roomwebapp/data/StaffRepository.java
@@ -0,0 +1,15 @@
+
+package com.frankmoley.lil.sbet.landon.roomwebapp.data;
+
+import com.frankmoley.lil.sbet.landon.roomwebapp.models.StaffMember;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StaffRepository extends JpaRepository<StaffMember, String> {
+}
+
+
+
+//package com.frankmoley.lil.sbet.landon.roomwebapp.data;
+//
+//public interface StaffRepository {
+//}
--- room-web-app/src/main/java/com/frankmoley/lil/sbet/landon/roomwebapp/models/StaffMember.java
+++ room-web-app/src/main/java/com/frankmoley/lil/sbet/landon/roomwebapp/models/StaffMember.java
@@ -1,13 +1,28 @@
 package com.frankmoley.lil.sbet.landon.roomwebapp.models;
 
+import jakarta.persistence.*;
+
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Table;
+import java.util.UUID;
+
+@Entity
+@Table(name="Employee")
 public class StaffMember {
 
+    @Id
+    @Column(name="EMPLOYEE_ID")
     private String employeeId;
+    @Column(name="FIRST_NAME")
     private String firstName;
+    @Column(name="LAST_NAME")
     private String lastName;
+    @Column(name="POSITION")
+    @Enumerated(EnumType.STRING)
     private Position position;
 
     public StaffMember() {
+        this.employeeId = UUID.randomUUID().toString();
     }
 
     public StaffMember(String employeeId, String firstName, String lastName, Position position) {
@@ -49,3 +64,58 @@ public class StaffMember {
         this.position = position;
     }
 }
+
+
+
+
+//package com.frankmoley.lil.sbet.landon.roomwebapp.models;
+//
+//public class StaffMember {
+//
+//    private String employeeId;
+//    private String firstName;
+//    private String lastName;
+//    private Position position;
+//
+//    public StaffMember() {
+//    }
+//
+//    public StaffMember(String employeeId, String firstName, String lastName, Position position) {
+//        this.employeeId = employeeId;
+//        this.firstName = firstName;
+//        this.lastName = lastName;
+//        this.position = position;
+//    }
+//
+//    public String getEmployeeId() {
+//        return employeeId;
+//    }
+//
+//    public void setEmployeeId(String employeeId) {
+//        this.employeeId = employeeId;
+//    }
+//
+//    public String getFirstName() {
+//        return firstName;
+//    }
+//
+//    public void setFirstName(String firstName) {
+//        this.firstName = firstName;
+//    }
+//
+//    public String getLastName() {
+//        return lastName;
+//    }
+//
+//    public void setLastName(String lastName) {
+//        this.lastName = lastName;
+//    }
+//
+//    public Position getPosition() {
+//        return position;
+//    }
+//
+//    public void setPosition(Position position) {
+//        this.position = position;
+//    }
+//}
